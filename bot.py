cead2cb6ca436935924b17098745c12fb029f207
svetomsk <svt2404@gmail.com>
2017-02-22 01:25:32 +0300

    AV-193 Notification buttons

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ import threading
 import re
 import time
 
+from telebot import types
+
 
 subscribers = []
 subscriptions_file = "./subscriptions"
@@ -26,6 +28,11 @@ def jira_hdl_trytask(prefix, regex, msg_text):
 def jira_hdl(message):
     return "\n".join(map(lambda p: jira_hdl_trytask(*p, message.text), jira_task_regex))
 
+keyboard = types.InlineKeyboardMarkup()
+okButton = types.InlineKeyboardButton(text="Буду", callback_data="ok")
+textButton = types.InlineKeyboardButton(text="Текст", callback_data="half_ok")
+denyButton = types.InlineKeyboardButton(text="Не буду", callback_data="fail")
+keyboard.add(okButton, textButton, denyButton);
 
 poker_marks = {}
 numbers_regex = re.compile('(\d+)')
@@ -73,6 +80,7 @@ def store_subscribers():
 def subscr_hdl(message):
     chat_id = message.chat.id
     msg_text = message.text
+    answ = ''
     global subscribers
     if msg_text.find('!subscribe') != 0:
         return ''
@@ -89,6 +97,7 @@ def subscr_hdl(message):
 def unsubscr_hdl(message):
     chat_id = message.chat.id
     msg_text = message.text
+    answ = ''
     global subscribers
     if msg_text.find('!unsubscribe') != 0:
         return ''
@@ -102,15 +111,37 @@ def unsubscr_hdl(message):
     return answ
 
 
-def send_alerts(alert_msg):
+def send_alerts(alert_msg, keyboard):    
     for chat_id in subscribers:
-        bot.send_message(chat_id, alert_msg)
+        bot.send_message(chat_id, alert_msg, reply_markup=keyboard)
+
+participants = {}
+
+def start_alert():
+    global participants
+    alive = []
+    text = []
+    absent = []
+    print(participants)
+    for key, value in participants.items():
+        if value == "ok":
+            alive.append(key)
+        elif value == "half_ok":
+            text.append(key)
+        elif value == "fail":
+            absent.append(key)
+    send_alerts("Стартуем!\n" + "Обещали прийти: " + ' '.join(alive) + "\n" + 
+        "В текстомов режиме: " + ' '.join(text) + "\n" + 
+        "Не придут: " + ' '.join(absent), None)
+    participants = []
 
 def send_alerts_thr():
     print("Alert sending thread started.")
-    schedule.every().day.at(alert_time_1h).do(lambda: send_alerts("Через час дебютнём!"))
-    schedule.every().day.at(alert_time_10min).do(lambda: send_alerts("Через 10 мин дебютнём!"))
-    schedule.every().day.at(alert_time_start).do(lambda: send_alerts("Стартуем!"))
+
+    schedule.every().day.at(alert_time_1h).do(lambda: send_alerts("Через час дебютнём!", None))
+    schedule.every().day.at(alert_time_10min).do(lambda: send_alerts("Через 10 мин дебютнём!", keyboard))
+    schedule.every().day.at(alert_time_start).do(start_alert)
+
     while 1:
         schedule.run_pending()
         time.sleep(1)
@@ -120,10 +151,19 @@ def send_alerts_thr():
 
 bot = telebot.TeleBot(config.token)
 bot_handlers = [poker_hdl, subscr_hdl, unsubscr_hdl, jira_hdl]
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    global participants
+    if call.message:
+        name = call.from_user.first_name
+        participants[name] = call.data
+
 @bot.message_handler(content_types=["text"])
 def handler(message):
     print("RECV: " + message.text)
     msg = ''.join(map(lambda h: h(message), bot_handlers)).strip()
+    
     if msg != "":
         bot.send_message(message.chat.id, msg)
 
